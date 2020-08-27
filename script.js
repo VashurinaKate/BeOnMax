987fd408807998dfad487ecb27e5173a305b761b
Kate <wasturina.ekatterina@gmail.com>
2020-08-27 22:10:23 +0300

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let money = prompt("Ваш бюджет на месяц?", "100$"),
-time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 let appData = {
     budget: money,
     expenses: {},
@@ -8,10 +8,28 @@ let appData = {
     timeData: time,
     savings: false
 };
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется?", ''),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдется?", '');
-    // appData.expenses.a1 = a2;
-    // appData.expenses.a3 = a4;
-    alert(appData.budget / 30);
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
+    }
+    
+};
+appData.moneyPerDay = appData.budget / 30;
+alert("ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("min level ")
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("middle");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("high level");
+} else {
+    console.log("error")
+}
